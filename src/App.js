5c5e6edd50f050355ac5a9526afba4a327ac8642
Kassidy R <kassidy.ralph@gmail.com>
2022-02-14 00:35:20 -0500

    updated Work component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-
+import Work from './components/Work';
+import ContactMe from './components/ContactMe';
 
 function App() {
   return (
@@ -17,39 +18,11 @@ function App() {
         </div>
 
         <div id="work">
-          <h2>Work</h2>
-          <p>
-            <a href="https://kassidyr.github.io/project1/" id="projects">
-              BirthdayFunFacts
-            </a>
-            <br />
-            An entertainment web application that utilizes server-side APIs to
-            provide motivation and interesting facts about a user's birthday
-          </p>
-          <p>
-            <a
-              href="https://glacial-everglades-31515.herokuapp.com/"
-              id="projects"
-            >
-              Goal Tracker
-            </a>
-            <br />
-            An easy-to-use application for creating and tracking goals
-          </p>
+        <Work></Work>
         </div>
 
         <div id="contact-me" class="contact-me">
-          <h2>Contact Me</h2>
-          <p>918.850.9870</p>
-          <p>
-            <a href="mailto:kassidy.ralph@gmail.com">kassidy.ralph@gmail.com</a>
-          </p>
-          <p>
-            <a href="https://www.linkedin.com/in/kassidy-ralph">LinkedIn</a>
-          </p>
-          <p>
-            <a href="https://github.com/kassidyr">GitHub</a>
-          </p>
+        <ContactMe></ContactMe>
         </div>
       </section>
     </body>
