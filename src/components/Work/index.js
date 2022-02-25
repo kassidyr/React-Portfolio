ca997eed312e2bd19deaf1aba16d24cffc665870
Kassidy Ralph <89705830+kassidyr@users.noreply.github.com>
2022-02-24 20:49:40 -0500

    updated work

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import birthdayFunFacts from "../../assets/images/BirthdayFunFacts.png";
 import goalTracker from "../../assets/images/GoalTracker.png";
+import spacebook from "../../assets/images/spacebook.png";
 
 function Work() {
     return(
         <div class="index, section">
         <h2>Work</h2>
         <img src={birthdayFunFacts} class="work-image" alt="Birthday Fun Facts" />
-        <p>
-               
+        <p>   
             <a href="https://kassidyr.github.io/project1/" id="projects">BirthdayFunFacts</a><br/>
             An entertainment web application that utilizes server-side APIs to provide motivation and interesting facts about a user's birthday
         </p>
@@ -17,6 +17,12 @@ function Work() {
         <p>
             <a href="https://glacial-everglades-31515.herokuapp.com/" id="projects">Goal Tracker</a><br/>
             An easy-to-use application for creating and tracking goals
+        </p>
+        <br></br>
+        <img src={spacebook} class="work-image" alt="spacebook" />
+        <p>
+            <a href="https://spacebook-social-network.herokuapp.com/" id="projects">spacebook</a><br/>
+            A web application that provides users with information regarding SpaceX launches and allows them to add comments and replies to the listed launch events
         </p>
      </div>  
     );
