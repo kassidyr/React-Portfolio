ca5da47185ebdc2fc6a828477c196a4952ce81a3
kassidyr <kassidy.ralph@gmail.com>
2022-02-28 13:23:08 -0500

    updated About Me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
     <img src={profilePic} class="float-left" alt="Kassidy Ralph portrait" />
     <h2>About Me</h2>
     <p>
-        I am a Financial Planning and Analysis Manager transitioning into a Web Developer role after completing the University of Miami Coding Boot Camp. I have over 5 years experience managing teams and working with executive leadership teams. My diverse experience includes management consulting, financial modeling, and corporate strategy for Fortune 500 companies.
+    I am a Web Developer transitioning from a professional background in Financial Planning and Analysis. I have over 5 years experience managing teams and working with executive leadership teams. My diverse experience includes management consulting, financial modeling, and corporate strategy for Fortune 500 companies. While completing my certificate in Full Stack Web Development from the University of Miami Boot Camp I developed a solid foundation in web development including building client-side applications using third-party APIs and creating single-page applications using the MERN stack. I look forward to leveraging my professional experience and technical skills to work as part of an engineering team.
     </p>
  </div>
 );
